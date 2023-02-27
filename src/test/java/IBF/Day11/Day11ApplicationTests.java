05a5d3f829531a84785c6f014ef9e18536f8c08c
jackson1010 <zinwin_1994@gmail.com>
2023-02-27 09:47:34 +0800

    first commit

---
+++ src/test/java/IBF/Day11/Day11ApplicationTests.java
@@ -0,0 +1,13 @@
+package IBF.Day11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
